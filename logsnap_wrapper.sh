bcbae07af294d88c43250001d06cf54ea965f38e
Mukhtar Saeed <mukeee6677@gmail.com>
2026-05-06 17:04:18 +0300

    chore: remove dev-only files from public repo (openspec, pm, agents, snap, matrix, tasks, scripts)

--- logsnap_wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "/Users/apple/Development/Smart log filter/CascadeProjects/windsurf-project"
-python3 -m mzgb "$@"
--- openspec/changes/mzgb/README.md
+++
@@ -1,3 +0,0 @@
-# mzgb
-
-Smart CLI tool for filtering and navigating very large log files with beautiful terminal output
